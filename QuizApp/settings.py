5e5357a1da90d063b0483b6a3b47852c995a3451
Alexander <youdoit@mail.ru>
2020-08-06 12:46:58 +0600

    Hiding secretkey and creating .gitignore file

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i6!o+k59u*sv5!2&b26gw06r!-k2#@*4aawe@-r89bt+^06dny'
+with open('secretkey.txt', 'r') as f:
+    SECRET_KEY = f.read()
+    f.close()
+    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
