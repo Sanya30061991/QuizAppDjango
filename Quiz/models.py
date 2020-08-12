33a030b63e9f9189ae8b3971c9057839e5978810
Alexander <youdoit@mail.ru>
2020-08-12 22:01:12 +0600

    Finished main page and half-created quiz model

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Quiz(models.Model):
+    title = models.TextField('Title of entire quiz')
+    author = models.TextField('Login of an author')
+    question_quantity = models.IntegerField('Amount of available questions')
+    category = models.TextField('Category of this quiz')
